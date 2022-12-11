1099bce50523ddefa814e19e205fdb8dc22151cb
ParamSurywashi <parmanandchared1997@gmail.com>
2022-12-11 20:03:28 +0530

    update error message

--- Js/JavaScript.js
+++ Js/JavaScript.js
@@ -2,18 +2,34 @@ function loadFunc(){
 	let x=window.innerWidth;
 	let y= window.innerHeight;
 	//document.getElementsByTagName("body")[0].style.backgroundImage="url('img/ccc.jpg')";
-	document.getElementsByTagName("body")[0].style.backgroundColor="#D69156";
+	document.getElementsByTagName("body")[0].style.backgroundColor="#CCF381";
+	//rgb(233 234 245)
 	document.getElementsByTagName("body")[0].style.backgroundSize = x+"px "+y+"px";
 	
+	if(window.localStorage.length == 0){
+		//alert("NULLLLLLL");
+	}else{
+		for(let items=0; items<window.localStorage.length; items++){
+			let areaitems=window.localStorage.getItem(items);
+			//console.log(areaitems);
+			//document.getElementById("comment").innerHTML=document.getElementById("comment").innerHTML+ ``+areaitems;
+		}
+	}
 }
 
 let commentDraw=null;
 function ClickFun(){
+	
+		 document.getElementById("errMsgName").innerText="";
+	  document.getElementById("errMsgArea").innerText="";
 	if(document.querySelectorAll('#area')[0].value == "" || document.querySelectorAll('#name')[0].value == ""){
-	  if(document.querySelectorAll('#area')[0].value == ""){
-	  alert("Please Comment in Text Field");
+	   if(document.querySelectorAll('#area')[0].value == "" && document.querySelectorAll('#name')[0].value == ""){
+	  document.getElementById("errMsgName").innerText="Please Type User Name";
+	  document.getElementById("errMsgArea").innerText="Please Comment in Text Field";
+	  }else if(document.querySelectorAll('#area')[0].value == ""){
+	  document.getElementById("errMsgArea").innerText="Please Comment in Text Field";
 	  }else if(document.querySelectorAll('#name')[0].value == ""){
-		  alert("Please Type User Name");
+	  document.getElementById("errMsgName").innerText="Please Type User Name";
 	  }
 	}else{
 	let area =createComment();
@@ -27,11 +43,22 @@ function ClickFun(){
  `<button id="likeBtn`+total_text+`" onClick="likeBtn(this)"><span id="likeCounter`+total_text+`"> </span>Like</button> `+
  `<button id="deleteBtn`+total_text+`" onclick=deleteCmd(this);>Delete Comment</button>`+
 ` <span id="date"> `+dateTime+` </span> <div id="Rplcmdbox`+total_text+`" class="Rplcmdbox"> </div>`
-+` <div id="replybox`+total_text+`" class="replybox"> </div></div>`;
++` <div id="replybox`+total_text+`" class="replybox"> </div> <span id="errMsgBox`+total_text+`" class="errMsgBox"> </span></div>  `;
 
 //document.querySelectorAll('#cmdText')[0].innerText=area;
 	//document.querySelectorAll('#date')[0].innerHTML=getDateTime();
 	document.querySelectorAll('#comment')[0].style.display="block";
+	
+	//localStorage.setItem(Usrname,document.getElementById("comment").innerHTML );
+	const nameandarea = {
+    name: Usrname,
+    area: document.getElementById("comment").innerHTML,
+}
+
+window.localStorage.setItem(total_text, JSON.stringify(nameandarea));
+
+	document.querySelectorAll('#name')[0].value = "";
+	document.querySelectorAll('#area')[0].value = "";
 	}
 }
 	
@@ -56,9 +83,9 @@ var datetime = currentdate.getDate() + "/"
 function replyBtn(data){
 let rplyId= data.id.slice(8);
 	if(document.querySelectorAll("[id=rpy"+rplyId+"]").length > 0) {
-    alert("Already have a Reply Box");
+    document.getElementById("errMsgBox"+rplyId).innerText="Already have a Reply Box";
      }else{
-	
+	document.getElementById("errMsgBox"+rplyId).innerText="";
 	document.getElementById("replybox"+rplyId).innerHTML=document.getElementById("replybox"+rplyId).innerHTML+
 	`<input type="textfield"  id="rpy`+rplyId+`" class="rpy"/> <br><br>`+
 `	<button id="rplCmdBtn`+rplyId+`" onclick="sendRply(this)">Send Reply</button> `;
@@ -69,12 +96,12 @@ let rplyId= data.id.slice(8);
 
 function sendRply(data){
 	let CmdId= data.id.slice(9);
-	let CmdData=document.querySelectorAll('.rpy')[0].value;
+	let CmdData=document.querySelectorAll('#rpy'+CmdId)[0].value;
 
 	if(CmdData==""){
-		alert("Please Reply in Box.... ");
+		document.getElementById("errMsgBox"+CmdId).innerText="Please Reply in Box.... ";
 	}else{
-	//let replyData=document.querySelectorAll('#Rplcmdbox')[0].value;
+	document.getElementById("errMsgBox"+CmdId).innerText="";
 	document.getElementById("replybox"+CmdId).innerHTML=``;
 	
   document.querySelectorAll('#replybox'+CmdId)[0].style.display="none";
@@ -82,6 +109,8 @@ function sendRply(data){
 	` <div id="spn`+CmdId+`" class="spn">`+CmdData+`</div>`;
 	
   document.querySelectorAll('#Rplcmdbox'+CmdId)[0].style.display="block";
+  
+  
 	}
   }
 function deleteCmd(data)
